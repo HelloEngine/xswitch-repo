d980587714addef9c0645211dc8be500dcc76f3e
musmusliu <musmusliu@tencent.com>
2022-10-16 23:10:26 +0800

    fix host to msys

--- packages/d/devkit-a64-gdb/xmake.lua
+++ packages/d/devkit-a64-gdb/xmake.lua
@@ -6,17 +6,15 @@ package("devkit-a64-gdb")
     add_versions("1.0.0", "e175365b760f3360b612400da4488ebd779b2b8e8c35f4a2163d2e66222cff08")
     set_description("Nintendo Switch aarch64-none-elf gdb")
     set_homepage("https://github.com/devkitPro")
-    if is_plat("cross") and is_arch("x86_64") then
-        add_urls("https://wii.leseratte10.de/devkitPro/devkitA64/devkitA64-gdb/$(version)", {version = function(version)
-            return versionUrls[version:shortstr()]
-        end})
-    end
+    add_urls("https://wii.leseratte10.de/devkitPro/devkitA64/devkitA64-gdb/$(version)", {version = function(version)
+        return versionUrls[version:shortstr()]
+    end})
 
     on_load(function(package)
         package:addenv("DEVKITA64_GDB", package:installdir())
     end)
 
-    on_install("cross|x86_64", function(package)
+    on_install("mingw@msys", "cross@msys", "linux@msys", function(package)
         os.cp("opt/devkitpro/devkitA64/bin", package:installdir())
         os.cp("opt/devkitpro/devkitA64/include", package:installdir())
         os.cp("opt/devkitpro/devkitA64/lib", package:installdir())
--- packages/s/switch-tools/xmake.lua
+++ packages/s/switch-tools/xmake.lua
@@ -6,17 +6,15 @@ package("switch-tools")
     add_versions("1.0.0", "91fa3e77d39840e83a6a0a1d7d1e5968bcb066edce9b3cdff917fb928dc591ff")
     set_description("Nintendo Switch tools")
     set_homepage("https://github.com/devkitPro")
-    if is_plat("cross") and is_arch("x86_64") then
-        add_urls("https://wii.leseratte10.de/devkitPro/switch/$(version)", {version = function(version)
-            return versionUrls[version:shortstr()]
-        end})
-    end
+    add_urls("https://wii.leseratte10.de/devkitPro/switch/$(version)", {version = function(version)
+        return versionUrls[version:shortstr()]
+    end})
 
     on_load(function(package)
         package:addenv("SWITCH_TOOLS", package:installdir())
     end)
 
-    on_install("cross|x86_64", function(package)
+    on_install("mingw@msys", "cross@msys", "linux@msys", function(package)
         os.cp("opt/devkitpro/tools/bin", package:installdir())
     end)
 
