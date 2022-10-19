862b98d8c75e875e3db19930e94420b10736fab2
musmusliu <musmusliu@tencent.com>
2022-10-19 21:22:00 +0800

    remove dynamic write xmake.lua

--- packages/b/bin2s/xmake.lua
+++ packages/b/bin2s/xmake.lua
@@ -4,28 +4,28 @@ package("bin2s")
     add_urls("https://github.com/HelloEngine/bin2s.git")
 
     on_install("@windows", "@msys", function(package)
-        io.writefile("xmake.lua", [[
-            target("bin2s")
-                set_plat("windows")
-                set_toolchains("mingw")
-                set_kind("binary")
-                on_install(function(target)
-                    if is_subhost("msys") then
-                        os.cp(target:targetfile() .. ".exe", target:installdir())
-                    else
-                        os.cp(target:targetfile(), target:installdir())
-                    end
-                end)
-                add_cxxflags("-std=c++11", "-Wextra", "-Wpedantic", "-Werror")
-                if is_mode("debug") then
-                    add_cxxflags("-g")
-                else
-                    add_cxxflags("-O3", "-DNDEBUG")
-                end
+        -- io.writefile("xmake.lua", [[
+        --     target("bin2s")
+        --         set_plat("windows")
+        --         set_toolchains("mingw")
+        --         set_kind("binary")
+        --         on_install(function(target)
+        --             if is_subhost("msys") then
+        --                 os.cp(target:targetfile() .. ".exe", target:installdir())
+        --             else
+        --                 os.cp(target:targetfile(), target:installdir())
+        --             end
+        --         end)
+        --         add_cxxflags("-std=c++11", "-Wextra", "-Wpedantic", "-Werror")
+        --         if is_mode("debug") then
+        --             add_cxxflags("-g")
+        --         else
+        --             add_cxxflags("-O3", "-DNDEBUG")
+        --         end
 
-                add_files("src/bin2s.cpp")
-                add_includedirs("deps/args")
-        ]])
+        --         add_files("src/bin2s.cpp")
+        --         add_includedirs("deps/args")
+        -- ]])
 
         import("package.tools.xmake").install(package, {})
     end)
--- packages/l/libnx/xmake.lua
+++ packages/l/libnx/xmake.lua
@@ -1,104 +1,100 @@
 package("libnx")
     set_description("Nintendo Switch AArch64-only userland library.")
     set_homepage("http://github.com/switchbrew")
-    --add_urls("http://github.com/switchbrew/libnx/archive/v$(version).tar.gz")
     add_urls("https://github.com/HelloEngine/libnx.git")
-
-    --add_versions("4.2.1", "6be7e3f1fe1768d101bc832348cd4369b25dee35e0551913b80a12fb596b021e")
-
     add_deps("switch-tools")
 
     on_install("cross@windows", "cross@msys", function(package)
         local configs = {}
-        io.writefile("nx/switch.specs", [[
-%rename link                old_link
+--         io.writefile("nx/switch.specs", [[
+-- %rename link                old_link
 
-*link:
-%(old_link) -T %:getenv(LIBNX /switch.ld) -pie --no-dynamic-linker --spare-dynamic-tags=0 --gc-sections -z text -z nodynamic-undefined-weak --build-id=sha1 --nx-module-name
+-- *link:
+-- %(old_link) -T %:getenv(LIBNX /switch.ld) -pie --no-dynamic-linker --spare-dynamic-tags=0 --gc-sections -z text -z nodynamic-undefined-weak --build-id=sha1 --nx-module-name
 
-*startfile:
-crti%O%s crtbegin%O%s --require-defined=main]])
+-- *startfile:
+-- crti%O%s crtbegin%O%s --require-defined=main]])
 
-        io.writefile("xmake.lua", string.format([[
-            add_rules("mode.debug", "mode.release")
-            set_plat("cross")
-            --set_version("%s")
-            toolchain("aarch64-none-elf")
-                set_kind("cross")
-                on_load(function (toolchain)
-                    toolchain:load_cross_toolchain()
-                end)
-            toolchain_end()
+--         io.writefile("xmake.lua", string.format([[
+--             add_rules("mode.debug", "mode.release")
+--             set_plat("cross")
+--             --set_version("%s")
+--             toolchain("aarch64-none-elf")
+--                 set_kind("cross")
+--                 on_load(function (toolchain)
+--                     toolchain:load_cross_toolchain()
+--                 end)
+--             toolchain_end()
 
-            add_repositories("xswitch-repo https://github.com/HelloEngine/xswitch-repo.git main")
-            add_requires("devkit-a64", "bin2s")
-            target("libnx")
-                set_basename("nx")
-                set_toolchains("aarch64-none-elf@devkit-a64")
-                set_kind("static")
-                add_packages("bin2s")
-                add_defines("LIBNX_NO_DEPRECATION", "__SWITCH__")
-                add_files("nx/source/**.s","nx/source/**.c")
-                add_includedirs("nx/data/","nx/include/", "nx/include/switch/", "nx/external/bsd/include/")
-                on_config(function(target)
-                    local binfile = os.curdir() .. "/nx/data/default_font.bin"
-                    assert(os.exists(binfile))
-                    local outdata, errdata
-                    if is_subhost("msys") then
-                        outdata, errdata = os.iorun("bin2s " .. binfile)
-                    else
-                        outdata, errdata = os.iorun("bin2s.exe " .. binfile)
-                    end
-                    io.writefile("nx/data/default_font_bin.s", outdata)
-                    io.writefile("nx/data/default_font_bin.h", [=[
-                        #pragma once
-                        extern const unsigned char default_font_bin[];
-                        extern const unsigned char default_font_bin_end[];
-                        extern const unsigned int default_font_bin_size;
-                    ]=])
-                    target:add("files", "nx/data/**.s")
-                end)
-                on_install(function(target)
-                    os.cp(target:targetfile(), target:installdir() .. "/lib/")
-                    os.cp(target:scriptdir() .. "/nx/include", target:installdir())
-                    os.cp(target:scriptdir() .. "/nx/external/bsd/include", target:installdir())
-                    os.cp(target:scriptdir() .. "/nx/default_icon.jpg", target:installdir())
-                    os.cp(target:scriptdir() .. "/nx/switch.specs", target:installdir())
-                    os.cp(target:scriptdir() .. "/nx/switch.ld", target:installdir())
-                end)
-                add_cflags("-g", 
-                    --"-Wall", 
-                    "-Werror",
-                    "-ffunction-sections", 
-                    "-fdata-sections", 
-                    "-march=armv8-a+crc+crypto", 
-                    "-mtune=cortex-a57",
-                    "-mtp=soft",
-                    "-fPIC", 
-                    "-ftls-model=local-exec")
-                add_cxxflags("-g", 
-                    --"-Wall", 
-                    "-Werror",
-                    "-ffunction-sections", 
-                    "-fdata-sections", 
-                    "-march=armv8-a+crc+crypto", 
-                    "-mtune=cortex-a57",
-                    "-mtp=soft",
-                    "-fPIC", 
-                    "-ftls-model=local-exec",
-                    "-fno-rtti",
-                    "-fno-exceptions",
-                    "-std=gnu++11")
-                add_asflags("-g",
-                    "-march=armv8-a+crc+crypto", 
-                    "-mtune=cortex-a57",
-                    "-mtp=soft",
-                    "-fPIC", 
-                    "-ftls-model=local-exec")
-                if is_mode("debug") then
-                    set_suffixname("d")
-                end
-        ]], package:version_str()))
+--             add_repositories("xswitch-repo https://github.com/HelloEngine/xswitch-repo.git main")
+--             add_requires("devkit-a64", "bin2s")
+--             target("libnx")
+--                 set_basename("nx")
+--                 set_toolchains("aarch64-none-elf@devkit-a64")
+--                 set_kind("static")
+--                 add_packages("bin2s")
+--                 add_defines("LIBNX_NO_DEPRECATION", "__SWITCH__")
+--                 add_files("nx/source/**.s","nx/source/**.c")
+--                 add_includedirs("nx/data/","nx/include/", "nx/include/switch/", "nx/external/bsd/include/")
+--                 on_config(function(target)
+--                     local binfile = os.curdir() .. "/nx/data/default_font.bin"
+--                     assert(os.exists(binfile))
+--                     local outdata, errdata
+--                     if is_subhost("msys") then
+--                         outdata, errdata = os.iorun("bin2s " .. binfile)
+--                     else
+--                         outdata, errdata = os.iorun("bin2s.exe " .. binfile)
+--                     end
+--                     io.writefile("nx/data/default_font_bin.s", outdata)
+--                     io.writefile("nx/data/default_font_bin.h", [=[
+--                         #pragma once
+--                         extern const unsigned char default_font_bin[];
+--                         extern const unsigned char default_font_bin_end[];
+--                         extern const unsigned int default_font_bin_size;
+--                     ]=])
+--                     target:add("files", "nx/data/**.s")
+--                 end)
+--                 on_install(function(target)
+--                     os.cp(target:targetfile(), target:installdir() .. "/lib/")
+--                     os.cp(target:scriptdir() .. "/nx/include", target:installdir())
+--                     os.cp(target:scriptdir() .. "/nx/external/bsd/include", target:installdir())
+--                     os.cp(target:scriptdir() .. "/nx/default_icon.jpg", target:installdir())
+--                     os.cp(target:scriptdir() .. "/nx/switch.specs", target:installdir())
+--                     os.cp(target:scriptdir() .. "/nx/switch.ld", target:installdir())
+--                 end)
+--                 add_cflags("-g", 
+--                     --"-Wall", 
+--                     "-Werror",
+--                     "-ffunction-sections", 
+--                     "-fdata-sections", 
+--                     "-march=armv8-a+crc+crypto", 
+--                     "-mtune=cortex-a57",
+--                     "-mtp=soft",
+--                     "-fPIC", 
+--                     "-ftls-model=local-exec")
+--                 add_cxxflags("-g", 
+--                     --"-Wall", 
+--                     "-Werror",
+--                     "-ffunction-sections", 
+--                     "-fdata-sections", 
+--                     "-march=armv8-a+crc+crypto", 
+--                     "-mtune=cortex-a57",
+--                     "-mtp=soft",
+--                     "-fPIC", 
+--                     "-ftls-model=local-exec",
+--                     "-fno-rtti",
+--                     "-fno-exceptions",
+--                     "-std=gnu++11")
+--                 add_asflags("-g",
+--                     "-march=armv8-a+crc+crypto", 
+--                     "-mtune=cortex-a57",
+--                     "-mtp=soft",
+--                     "-fPIC", 
+--                     "-ftls-model=local-exec")
+--                 if is_mode("debug") then
+--                     set_suffixname("d")
+--                 end
+--         ]], package:version_str()))
         import("package.tools.xmake").install(package, configs)
     end)
 
