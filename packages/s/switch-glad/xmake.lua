efe39b0f3a0a77fa45eafd01dd23bc5ff1d2efd7
musmus <musmus9405@qq.com>
2022-11-16 10:47:21 +0800

    Update xmake.lua

--- packages/s/switch-glad/xmake.lua
+++ packages/s/switch-glad/xmake.lua
@@ -3,6 +3,7 @@ package("switch-glad")
     set_homepage("https://github.com/HelloEngine/switch-glad")
 
     add_versions("1.0.0", "1000f80fcc559e0ec4ce3ef808620fd0266ef519a73ace72ffe1c18c36943b90")
+    add_versions("1.0.1", "a8834e28ce5a5ce3e0e6678c32203f1c0a411fec0f8a00a87988a58c5c24c469")
     add_urls("https://github.com/HelloEngine/switch-glad/releases/download/$(version)/switch-glad.zip")
 
     add_deps("switch-mesa")
